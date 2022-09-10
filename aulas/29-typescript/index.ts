3e2a58f94ffe740d6ad76740741203ec6f3a7f8d
Mateus-Nunes <mateusnunes620@gmail.com>
2022-09-09 21:11:45 -0300

    add-data

--- aulas/29-typescript/index.ts
+++ aulas/29-typescript/index.ts
@@ -1,1 +1,51 @@
-const userName = `Mateus`
+function sum(a: number, b: number) {
+  return a + b
+}
+
+let user: string = 'Mateus'
+
+let valor = 5
+
+let numbers: number[] = [1, 2, 3, 4, 5, 6]
+
+let users: Array<string> = ['Mateus', 'Adrielly']
+
+function showMessages(message: string): void {
+  console.log (message)
+}
+
+function printUserId(id: number | string) {
+  console.log (`O ID do usuário é ${id}`)
+}
+
+/*
+S => state
+T => type
+K => Key
+V => value
+E => element
+*/
+
+function useState<T extends number | string = number>() {
+  let state: T
+
+  function get() {
+    return state
+  }
+
+  function set(newValue: T) {
+    state = newValue
+  }
+
+  return { get, set }
+}
+
+let newState = useState<number>()
+
+newState.get()
+newState.set(123)
+
+type IdType = string | number | undefined
+
+let userId: IdType
+let adminId: IdType
