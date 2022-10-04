80737ce37bdb02dd0095b0955086c29977b1b1b5
Mateus-Nunes <mateusnunes620@gmail.com>
2022-10-04 17:00:02 -0300

    estruturas-stack

--- aulas/20-estrutura-de-dados/segundo/stack.js
+++ aulas/20-estrutura-de-dados/segundo/stack.js
@@ -6,4 +6,37 @@ class Stack {
     this.top = -1
   }
 
+  push(value) {
+    this.top++
+    this.data[this.top] = value
+    return this.data
+  }
+
+  pop() {
+    if (this.top < 0) return undefined
+    const poppedTop = this.data[this.top]
+    delete this.data[this.top]
+    this.top--
+    return poppedTop
+  }
+
+  peek() {
+    return this.top >= 0 ? this.data[this.top] : undefined
+  }
+
+  size() {
+    let size = this.top
+    size++
+    return size
+  }
+
 }
+
+const pilha = new Stack()
+pilha.push('Primeiro elemento')
+console.log(pilha.size(), pilha.peek())
+pilha.push('Segundo')
+pilha.push('Terceiro')
+console.log(pilha.size(), pilha.peek())
+pilha.pop()
+console.log(pilha.size(), pilha.peek())
