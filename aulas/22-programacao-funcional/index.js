c8bbf2df73edc8888b333d1f708336fb0afd32d7
Mateus-Nunes <mateusnunes620@gmail.com>
2022-10-07 20:21:50 -0300

    lista-for

--- aulas/22-programacao-funcional/index.js
+++ aulas/22-programacao-funcional/index.js
@@ -1,11 +1,13 @@
 // imperativo
 let number = 2
 
+/*
 function square() {
   return number * number
 }
 
 number = square()
+*/
 
 // Declarativa
 const quadrado = n => n * n
@@ -22,14 +24,46 @@ const cart = {
 // good
 const newCart = {...cart, quantity: 3}
 
-// reactJS
-const [amount, setAmount] = useState(0)
-
-// bad
-// amount = 2
-
-// good
-setAmount(2)
-
-
 // Independente: nada aqui afeta o mundo externo
+const random = (number, Math) => {
+  Math.floor(Math.random() * number)
+}
+
+// recursive
+const fatorial = x => {
+  
+  if (x < 2) {
+    return 1
+  }
+
+  return x * fatorial(x - 1)
+}
+
+//Puras
+let person = {
+  name: 'Rafael Camarada',
+  age: 'Jovem'
+}
+
+const changePersonName = (person, name) => ({...person, name})
+
+//first-class function
+const sayMyName = () => console.log('Mateus')
+const runFunction = fn => fn()
+
+//runFunction(sayMyName)
+
+// higher-order function
+const numbers = [2, 4, 8, 16]
+const square = n => n * n
+const squaredNumber = numbers.map(square)
+
+const pause = wait => fn => setTimeout(fn, wait)
+
+pause(600)(() => console.log('waiting 600ms'))
+
+const wait200 = pause(200)
+const wait1000 = pause(1000)
+
+wait200(() => console.log('waiting 200ms'))
+wait1000(() => console.log('waiting 1000ms'))
